ba5b118ab2eeb0acb55b856a4d6948c145de41ee
Doreen970 <doreenwanyama20@gmail.com>
2023-08-11 10:11:01 -0700

    made some correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ import sys
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    total_args = sum(int(arg) for arg in agrv)
+    total_args = sum(int(arg) for arg in argv)
     print(total_args)
