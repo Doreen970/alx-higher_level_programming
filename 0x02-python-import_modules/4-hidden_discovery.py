321ba3b74d6a821a0b671a95ae577275e1e97a6f
Doreen970 <doreenwanyama20@gmail.com>
2023-08-11 10:32:28 -0700

    Write a program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally).
    You should print one name per line, in alpha order
    You should print only names that do not start with __
    Your code should not be executed when imported
    Make sure you are running your code in Python3.8.x (hidden_4.pyc has been compiled with this version)

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.8
+
+import dis
+import importlib.util
+import sys
+
+if __name__ == "__main__":
+    if sys.version_info < (3, 8):
+        print("This script requires Python 3.8.x")
+        sys.exit(1)
+    spec = importlib.util.spec_from_file_location("hidden_module", "hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    hidden_names = []
+    for name in dir(module):
+        if not name.startswith("__"):
+            hidden_names.append(name)
+
+    hidden_names.sort()
+    for name in hidden_names:
+        print(name)
