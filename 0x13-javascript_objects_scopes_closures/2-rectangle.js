d12df9fe24a20c985c8f1d5c64f10274da4e8f95
Doreen970 <doreenwanyama20@gmail.com>
2024-04-24 15:22:00 -0700

    made constructor to return undefined

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,13 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
+      // If w or h is equal to 0 or not a positive integer, set width and height to undefined
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
