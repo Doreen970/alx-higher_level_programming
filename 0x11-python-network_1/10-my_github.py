f231e2cd2e26b32802977a8ff78f5176c5086711
Doreen970 <doreenwanyama20@gmail.com>
2024-05-11 13:48:13 -0700

    some changes

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 ''' script that takes your GitHub credentials (username and password),
-and uses the GitHub API to display your id
-'''
-import sys
-from requests import get, exceptions
-
-
-if __name__ == '__main__':
-    token = ''
-    _, user, psw = sys.argv
-    url = f'https://api.github.com/users/{user}'
-    header = {
-            "Authorization": f"Bearer {psw}",
-            'Accept': 'application/vnd.github+json',
-    }
+and uses th
 
     try:
         req = get(url, headers=header)
