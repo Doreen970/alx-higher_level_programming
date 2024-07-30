5e44f305b16032cd5fd2c3eb7ad35378be85ca54
Doreen970 <doreenwanyama20@gmail.com>
2024-07-30 05:55:02 -0700

    some changes

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
-// prints characters of star wars movie in right order
+// prints characters of Star Wars movie in right order
 
-const request = require('request');
+const request = require('request-promise');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+async function getCharacterNames(urls) {
+  for (const url of urls) {
+    try {
+      const body = await request(url);
+      const character = JSON.parse(body);
+      console.log(character.name);
+    } catch (error) {
+      console.log(error);
     }
   }
-});
+}
+
+request(url)
+  .then(body => {
+    const content = JSON.parse(body);
+    return content.characters;
+  })
+  .then(getCharacterNames)
+  .catch(error => {
+    console.log(error);
+  });
